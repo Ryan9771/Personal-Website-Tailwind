f35bd1673530d06fd637e43e09cc6e2995bf78fc
ryan <ryan.patel21@imperial.ac.uk>
2022-12-04 17:41:33 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+    minWidth: {
+      '300': '300px',
+    }
+  },
+  plugins: [],
+}
