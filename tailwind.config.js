be35510bf38b62e2e89caa0150285753aec3f17c
ryan <ryan.patel21@imperial.ac.uk>
2022-12-18 22:27:36 +0000

    Mobile format for home-about

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
-    minWidth: {
-      '280': '280px',
-      '290': '290px',
-      '300': '300px',
-    },
-    colors: {
-      'primary': '#FEFDE8',
-      'secondary': '#003049',
-      'header': '#7e0306',
-      'link': '#2d2f83',
+    extend: {
+      minWidth: {
+        '280': '280px',
+        '290': '290px',
+        '300': '300px',
+      },
+      colors: {
+        'primary': '#FEFDE8',
+        'secondary': '#003049',
+        'header': '#7e0306',
+        'link': '#2d2f83',
+      },
     },
   },
   plugins: [],
