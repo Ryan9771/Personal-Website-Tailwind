6063136d4c59745768de428d2f9373d2f5daf27e
ryan <ryan.patel21@imperial.ac.uk>
2022-12-20 20:46:53 +0000

    Calculator modal done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./src/*.html", "./src/Modals/*.html"],
   theme: {
     extend: {
       minWidth: {
