77621bd889649e177e075ca3f027aa33650e504a
ryan <ryan.patel21@imperial.ac.uk>
2022-12-23 20:06:57 +0000

    Completed arrow and navbar integration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// Gets both about containers in a list
+// Gets both about contact containers in a list
 const aboutCtns = [...document.querySelectorAll(".about-ctn")];
 
 // The buttons for the about section
@@ -6,26 +6,64 @@ const tab = document.querySelector(".next-btn-1");
 const arrow = document.querySelector(".next-btn-2");
 const arrowIcon = document.getElementById("next-btn-icon");
 
-let aboutDiv = 0;
+// Keeps track which of about / contact is shown
+let cnt = 0;
 
 // Default state 
 aboutCtns[0].classList.remove("hidden");
 aboutCtns[0].classList.add("flex");
 
+// Switches to the right about state with correct arrow direction
 arrow.addEventListener("click", function() {
-    aboutCtns[aboutDiv].classList.remove("flex");
-    aboutCtns[aboutDiv].classList.add("hidden");
+    aboutCtns[cnt].classList.remove("flex");
+    aboutCtns[cnt].classList.add("hidden");
     let rotate;
-    if (aboutDiv == 0) {
-        aboutDiv++;
+    if (cnt == 0) {
+        cnt++;
         rotate = "-180deg";
     } else {
-        aboutDiv = 0;
+        cnt = 0;
         rotate = "0deg";
     }
-    aboutCtns[aboutDiv].classList.remove("hidden");
-    aboutCtns[aboutDiv].classList.add("flex");
+    aboutCtns[cnt].classList.remove("hidden");
+    aboutCtns[cnt].classList.add("flex");
     arrowIcon.style.transform = `rotate(${rotate})`;
 });
 
+// Handles synchronisation of navbar about / contact buttons pressed and the 
+//  arrow, and the relevant section
+function toggleAboutContact(count) {
+    if (aboutCtns[count].classList.contains("hidden")) {
+        aboutCtns[count].classList.remove("hidden");
+        aboutCtns[count].classList.add("flex");
+    }
+    // The about button has been clicked
+    if (count === 0) {
+        if (aboutCtns[1].classList.contains("flex")) {
+            aboutCtns[1].classList.remove("flex");
+            aboutCtns[1].classList.add("hidden"); 
+        }
+        arrowIcon.style.transform = 'rotate(0deg)';
+    } else {
+        if (aboutCtns[0].classList.contains("flex")) {
+            aboutCtns[0].classList.remove("flex");
+            aboutCtns[0].classList.add("hidden"); 
+        }
+        arrowIcon.style.transform = 'rotate(-180deg)';    
+    }
+}
+
+// Deals with arrow direction when the navabar button is clicked
+const aboutNavBtn = document.getElementById("about-nav");
+const contactNavBtn = document.getElementById("contact-nav");
+
+aboutNavBtn.addEventListener("click", () => {
+    cnt = 0;
+    toggleAboutContact(cnt);
+})
+
+contactNavBtn.addEventListener("click", () => {
+    cnt = 1;
+    toggleAboutContact(cnt);
+})
 
