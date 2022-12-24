e5a7e79edc350ea9f03369354a45699081fa422f
ryan <ryan.patel21@imperial.ac.uk>
2022-12-24 10:41:44 +0000

    Added all pictures and custom width to each modal. Website done?

--- src/Modals/modal.js
+++ src/Modals/modal.js
@@ -1,25 +1,49 @@
 // ========= PROJECT MODALS ======== \\ 
 
+// Maintains the current index of shown image
 let imgCnt = 0;
 
-const rightArrows = [...document.querySelectorAll(".modal-arrow-right")];
+// Retrieves the right arrow 
+const rightArrow = document.querySelector(".modal-arrow-right");
 
-for (let i = 0; i < rightArrows.length; i++) {
-    let proj = rightArrows[i].getAttribute("project");
-    rightArrows[i].addEventListener("click", function() {
-        const projImages = [...document.querySelectorAll("." + proj)];
-        imgCnt++;
-        if (imgCnt >= projImages.length) {
-            imgCnt = 0;
-        }
-        for (let j = 0; j < projImages.length; j++) {
-            if (imgCnt == j) {
-                if (projImages[j].classList.contains("hidden")) {
-                    projImages[j].classList.remove("hidden");
-                }
-            } else if (!projImages[j].classList.contains("hidden")) {
-                projImages[j].classList.add("hidden");
+// Iterates through the images based on the respective project
+let proj = rightArrow.getAttribute("project");
+rightArrow.addEventListener("click", function() {
+    const projImages = [...document.querySelectorAll("." + proj)];
+    imgCnt++;
+    if (imgCnt >= projImages.length) {
+        imgCnt = 0;
+    }
+    for (let j = 0; j < projImages.length; j++) {
+        if (imgCnt === j) {
+            if (projImages[j].classList.contains("hidden")) {
+                projImages[j].classList.remove("hidden");
             }
+        } else if (!projImages[j].classList.contains("hidden")) {
+            projImages[j].classList.add("hidden");
         }
-    })
-}
+    }
+})
+
+
+// Retrieves the left arrow
+const leftArrow = document.querySelector(".modal-arrow-left");
+
+// Iterates through the images based on the respective project
+proj = leftArrow.getAttribute("project");
+leftArrow.addEventListener("click", function() {
+    const projImages = [...document.querySelectorAll("." + proj)];
+    imgCnt--;
+    if (imgCnt < 0) {
+        imgCnt = projImages.length - 1;
+    }
+    for (let j = 0; j < projImages.length; j++) {
+        if (imgCnt === j) {
+            if (projImages[j].classList.contains("hidden")) {
+                projImages[j].classList.remove("hidden");
+            }
+        } else if (!projImages[j].classList.contains("hidden")) {
+            projImages[j].classList.add("hidden");
+        }
+    }
+})
